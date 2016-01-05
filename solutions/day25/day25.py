dda78ea9eab15b79a25854206d45e2f42acca293
Bryan Johnson <d.bryan.johnson@gmail.com>
2016-01-05 00:30:37 -0500

    minor cleanup

--- solutions/day25/day25.py
+++ solutions/day25/day25.py
@@ -2,19 +2,23 @@ def fibonacci(i):
     # can't do "classic" recursive fibonacci here - i is too large
     s = 1
     for c in xrange(i):
-        s = s + c + 1
+        s += c + 1
     return s
 
 
 def row_col_to_idx(row, col):
+    row -= 1
+    col -= 1
     return fibonacci(row + col) + col
+
 
 target_row = 2981
 target_col = 3075
-idx = row_col_to_idx(target_row - 1, target_col - 1)
+idx = row_col_to_idx(target_row, target_col)
 
 code = 20151125
 for _ in xrange(idx - 1):
-    code = (code * 252533) % 33554393
+    code *= 252533
+    code %= 33554393
 
 print 'part 1:', code
