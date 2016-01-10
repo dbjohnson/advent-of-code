a3f6375495bdc20fd564655f23df8ac1799b3dbb
Bryan Johnson <d.bryan.johnson@gmail.com>
2016-01-10 12:35:05 -0500

    non-numpy solution

--- solutions/day01/solution.py
+++ solutions/day01/solution.py
@@ -1,7 +1,11 @@
-import numpy as np
-
 with open('input.txt', 'r') as fh:
     moves = [1 if m == '(' else -1 for m in fh.readline().strip()]
 
 print 'part 1', sum(moves)
-print 'part 2', np.cumsum(moves).tolist().index(-1) + 1
+
+pos = 0
+for i, move in enumerate(moves):
+    pos += move
+    if pos == -1:
+        print 'part 2', i + 1
+        break
