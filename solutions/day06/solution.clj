0ffa854f8babac2b89ad79ffe279962878404d68
Bryan Johnson <d.bryan.johnson@gmail.com>
2016-01-11 20:03:36 -0500

    refactor for clarity

--- solutions/day06/solution.clj
+++ solutions/day06/solution.clj
@@ -1,58 +1,61 @@
+(defn cartesian-product
+  [xmin xmax ymin ymax]
+  (for [x (range xmin xmax)
+        y (range ymin ymax)]
+    [x y]))
+
+
 (defn parse-instruction
   [instruction]
   (let [pattern #"(toggle|turn off|turn on) ([0-9]+),([0-9]+) through ([0-9]+),([0-9]+)"
         matches (re-matches pattern instruction)
         xs (map read-string (map #(nth matches %) [2 4]))
-        ys (map read-string (map #(nth matches %) [3 5]))]
+        ys (map read-string (map #(nth matches %) [3 5]))
+        [xmin xmax] (sort xs)
+        [ymin ymax] (sort ys)]
         {:action (nth matches 1)
-         :x_from (apply min xs)
-         :y_from (apply min ys)
-         :x_to (apply max xs)
-         :y_to (apply max ys)})) 
+         :indexes (cartesian-product xmin (inc xmax) ymin (inc ymax))})) 
 
 (def instructions (->> (slurp "input.txt")
                        (clojure.string/split-lines)
                        (map parse-instruction)))
 
 (defn process-instruction
-  [instruction lights fnmap]
-  (let [action (fnmap (:action instruction))
-        idxs (for [x (range (:x_from instruction) (inc (:x_to instruction)))
-                   y (range (:y_from instruction) (inc (:y_to instruction)))]
-               [x y])]
-    (loop [[idx & more] idxs
+  [instruction lights action->fn]
+  (let [fn (action->fn (:action instruction))]
+    (loop [[idx & more] (:indexes instruction)
            lights lights]
       (if idx 
         (let [[x y] idx
               loc {:x x :y y}
               curr-val (or (lights loc) 0)
-              next-val (action curr-val)]
+              next-val (fn curr-val)]
             (recur more 
                    (assoc lights loc next-val)))
         lights))))
 
 (defn solve
-  [instructions fnmap]
+  [instructions action->fn]
   (loop [[instruction & more] instructions
          lights {}]
     (if instruction
       (recur more
-             (process-instruction instruction lights fnmap))
+             (process-instruction instruction lights action->fn))
       lights)))
 
 (defn part1
   [instructions]
-  (let [fnmap {"toggle" #(if (zero? %) 1 0) 
-               "turn on" (constantly 1)
-               "turn off" (constantly 0)}]
-    (reduce + (vals (solve instructions fnmap)))))
+  (let [action->fn {"toggle" #(if (zero? %) 1 0) 
+                    "turn on" (constantly 1)
+                    "turn off" (constantly 0)}]
+    (reduce + (vals (solve instructions action->fn)))))
 
 (defn part2
   [instructions]
-  (let [fnmap {"toggle" #(+ % 2) 
-               "turn on" #(+ % 1)
-               "turn off" #(max 0 (- % 1))}]
-    (reduce + (vals (solve instructions fnmap)))))
+  (let [action->fn {"toggle" #(+ % 2) 
+                    "turn on" #(+ % 1)
+                    "turn off" #(max 0 (- % 1))}]
+    (reduce + (vals (solve instructions action->fn)))))
 
 (println "part 1:" (part1 instructions))
 (println "part 2:" (part2 instructions))
