c6824bae28f851ecac812bfcaed6727467b43bc7
Bryan Johnson <d.bryan.johnson@gmail.com>
2016-01-26 09:05:21 -0500

    simplified python solution

--- solutions/day10/solution.py
+++ solutions/day10/solution.py
@@ -1,16 +1,9 @@
-def look_and_say(x):
-    c = x[0]
-    n = 1
-    expanded = ''
-    for cc in x[1:]:
-        if cc != c:
-            expanded += '{}{}'.format(n, c)
-            c = cc
-            n = 0
-        n += 1
-    expanded += '{}{}'.format(n, c)
-    return expanded
+import re
 
+
+def look_and_say(x):
+    return ''.join('{}{}'.format(len(c.group()), c.group()[0])
+                   for c in re.finditer('([0-9])\\1*', x))
 
 for part, repeat in (('part 1', 40), ('part 2', 50)):
     x = '1113122113'
